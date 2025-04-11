b0e52e29b633720330ccd0d415d26552292817ec
34sea <domingosmartinho67@gmail.com>
2025-04-11 11:53:07 +0200

    add new layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(MyApp());
@@ -93,6 +96,16 @@ class Menu extends StatelessWidget {
                   },
               child: Text("Tela 3"),
             ),
+            ElevatedButton(
+              onPressed:
+                  () => {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => ApiConsumer()),
+                    ),
+                  },
+              child: Text("Api"),
+            ),
           ],
         ),
       ),
@@ -131,9 +144,214 @@ class TerceiraTela extends StatelessWidget {
               onPressed: () => {Navigator.pop(context)},
               child: Text("Voltar"),
             ),
+            CountState(title: "Counter"),
+            ChangeColor(title: 'Color'),
           ],
         ),
       ),
+    );
+  }
+}
+
+//States
+
+class CountState extends StatefulWidget {
+  const CountState({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<CountState> createState() => _MyCountState();
+}
+
+class _MyCountState extends State<CountState> {
+  int _counter = 0;
+
+  void incremente() {
+    setState(() {
+      _counter++;
+      print('$_counter');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text('Counter'),
+        Text('$_counter'),
+        ElevatedButton(
+          onPressed: () => {incremente()},
+          child: Text('Incrementar'),
+        ),
+      ],
+    );
+  }
+}
+
+class ChangeColor extends StatefulWidget {
+  ChangeColor({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<ChangeColor> createState() => _ChangeStateColor();
+}
+
+class _ChangeStateColor extends State<ChangeColor> {
+  var _colorItems = Colors.blue;
+  bool _colorIsChange = false;
+  void changeColor() {
+    setState(() {
+      if (!_colorIsChange) {
+        _colorItems = Colors.red;
+        _colorIsChange = !_colorIsChange;
+      } else {
+        _colorItems = Colors.amber;
+        _colorIsChange = !_colorIsChange;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text('Color', style: TextStyle(color: _colorItems)),
+        ElevatedButton(onPressed: () => {changeColor()}, child: Text('Change')),
+      ],
+    );
+  }
+}
+
+//APi
+class ApiConsumer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Api')),
+      body: Center(
+        child: Column(
+          children: [Text("Api"), ApiConsumerState(title: 'api'), PostsPage()],
+        ),
+      ),
+    );
+  }
+}
+
+class ApiConsumerState extends StatefulWidget {
+  ApiConsumerState({super.key, required this.title});
+
+  final String title;
+
+  State<ApiConsumerState> createState() => _ApiConsumerStateShow();
+}
+
+class _ApiConsumerStateShow extends State<ApiConsumerState> {
+  Future<void> readeData() async {
+    print('object');
+    try {
+      var BASE_URL = Uri.parse("https://jsonplaceholder.typicode.com/posts");
+      var response = await http.get(BASE_URL);
+      if (response.statusCode == 200) {
+        List<dynamic> data = jsonDecode(response.body) as List<dynamic>;
+
+        for (final dados in data) {
+          print(dados['id']);
+        }
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  var _teste = 0;
+  void incremente() {
+    setState(() {
+      _teste++;
+      readeData();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return (Column(
+      children: [
+        Text('$_teste'),
+
+        ElevatedButton(onPressed: () => {incremente()}, child: Text('Click')),
+      ],
+    ));
+  }
+}
+
+class PostsPage extends StatefulWidget {
+  @override
+  _PostsPageState createState() => _PostsPageState();
+}
+
+class _PostsPageState extends State<PostsPage> {
+  Future<List<dynamic>> fetchPosts() async {
+    final response = await http.get(
+      Uri.parse('https://jsonplaceholder.typicode.com/posts'),
+    );
+
+    if (response.statusCode == 200) {
+      return json.decode(response.body);
+    } else {
+      throw Exception('Erro ao carregar os posts');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [Text('Api')]);
+    // return Scaffold(
+    //   appBar: AppBar(title: Text('Posts')),
+    //   body: FutureBuilder<List<dynamic>>(
+    //     future: fetchPosts(),
+    //     builder: (context, snapshot) {
+    //       if (snapshot.connectionState == ConnectionState.waiting) {
+    //         return Center(child: CircularProgressIndicator());
+    //       } else if (snapshot.hasError) {
+    //         return Center(child: Text('Erro: ${snapshot.error}'));
+    //       } else if (snapshot.hasData) {
+    //         final posts = snapshot.data!;
+    //         return ListView.builder(
+    //           itemCount: posts.length,
+    //           itemBuilder: (context, index) {
+    //             final post = posts[index];
+    //             return ListTile(
+    //               title: Text(post['title']),
+    //               subtitle: Text(post['body']),
+    //             );
+    //           },
+    //         );
+    //       } else {
+    //         return Center(child: Text('Nenhum dado encontrado'));
+    //       }
+    //     },
+    //   ),
+    // );
+  }
+}
+
+class ListeWidget extends StatelessWidget {
+  final List<String> entries = <String>['A', 'B', 'C'];
+  final List<int> colorCodes = <int>[600, 500, 100];
+
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      padding: const EdgeInsets.all(8),
+      itemCount: entries.length,
+      itemBuilder: (BuildContext context, int index) {
+        return Container(
+          height: 50,
+          color: Colors.amber[colorCodes[index]],
+          child: Center(child: Text('Entry ${entries[index]}')),
+        );
+      },
+      separatorBuilder: (BuildContext context, int index) => const Divider(),
     );
   }
 }
